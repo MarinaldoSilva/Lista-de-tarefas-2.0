628cb25f15f7c01a9d774786087608cfc3ca0762
Mario <marinaldo12@hotmail.com>
2025-09-28 21:45:23 -0300

    adaptando o serializer para lidar com o campo dono

--- core/serializers.py
+++ core/serializers.py
@@ -3,9 +3,15 @@ from .models import Tarefa
 
 
 class TarefaSerializer(serializers.ModelSerializer):
+      
+    """
+    essa informação vem do banco(serializar objs python p/ json), não vem do cliente, retorna para o cliente a informação do dono.username, ou seja, o nome do criador da nota, após comparação do token que vem no header no campo Authorization, a consulta no banco é feito e se for um user valido, vai buscar o nome do dono que detem deteminado token
+    """
+    dono = serializers.ReadOnlyField(source = 'dono.username')
+
     class Meta:
         model = Tarefa
-        fields = ['id', 'descricao', 'concluido', 'data_criacao', 'prioridade']
-        read_only_fields = ['id', 'data_criacao']
+        fields = ['id', 'dono','descricao', 'concluido', 'data_criacao', 'prioridade']
+        read_only_fields = ['id', 'data_criacao', 'dono']
 
    
